bc5ec6da0e389e6d12afc3c6b317db7545160b1b
Johndhee <Johndheeballesteros13@gmail.com>
2021-04-28 16:49:44 -0700

    zach & johndhee finsih

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react'
+
+export default class CoordinatesButton extends Component{
+    render(){
+        return (
+            <button onClick={this.clickHandler}>
+                coordinatesButton
+            </button>
+        )
+    }
+
+    clickHandler = event =>{
+        let position = [event.clientX, event.clientY]
+        this.props.onReceiveCoordinates(position)
+
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,18 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react'
+
+export default class DelayedButton extends Component{
+    render(){
+        return(
+            <button onClick={this.clickHandler}>
+                delayedButton
+            </button>
+        )
+    }
+
+    clickHandler = event =>{
+        console.log(event)
+        event.persist()
+        setTimeout(()=>this.props.onDelayedClick(event), this.props.delay)
+    }
+}
